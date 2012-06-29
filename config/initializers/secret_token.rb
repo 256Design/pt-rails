b695c91dd16638b1ed363f1d485b0939b6e37d1e
Spencer Oberstadt <soberstadt@gmail.com>
2012-06-28 23:11:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PtRails::Application.config.secret_token = '6a5e50ecdf96989936ea186d1bd7c2f35a0ec5fdd2847db83c0e5dbc2afe19903b581e4d489f4476c3e4a570411d6b4e5555d221eee4cbf6c9e1f4da0473539c'
