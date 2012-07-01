75377117e43852814382f67807b4e7bde12dc6d2
Spencer Oberstadt <soberstadt@gmail.com>
2012-07-01 17:00:42 -0400

    more tests/cleanup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,18 @@ describe "Static Pages" do
 
   subject { page }
  
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', :text => heading) }
+    it { should have_selector('title', :text => full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    
+    let(:heading)     { 'Project Transparency' }
+    let(:page_title)  { '' }
 
-    it { should have_selector('h1', :text => 'Project Transparency') }
-    it { 
-      should have_selector('title', 
-                           :text => full_title('')) 
-    }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: ' | Home') }
   end
   
@@ -34,5 +38,20 @@ describe "Static Pages" do
 
     it { should have_selector('h1', :text => 'Help') }
     it { should have_selector('title', :text => full_title('Help')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Make an account"
+    page.should have_selector 'title', text: full_title('Sign Up')
+    click_link "Project Transparency"
+    page.should have_selector 'title', text: full_title('')
   end
 end
