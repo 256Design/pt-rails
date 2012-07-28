1b3acc973129b2078eddfb38eac3288b822bc547
Spencer Oberstadt <soberstadt@gmail.com>
2012-07-28 19:18:07 -0400

    add save as lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50}
